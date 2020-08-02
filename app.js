4ee5a121b40f71f34140880b5fce7cf14be689c1
lonan rynn-pring <lonanrynnpring@gmail.com>
2020-08-02 22:31:21 +0100

    width fix

--- app.js
+++ app.js
@@ -259,11 +259,7 @@ const colours = [
     'cyan',
 ]
 
-<<<<<<< HEAD
 const arena = createMatrix(10, 20);
-=======
-const arena = createMatrix(20, 20);
->>>>>>> singlePlayer
 
 const player = {
     pos: {x: 0, y: 0},
